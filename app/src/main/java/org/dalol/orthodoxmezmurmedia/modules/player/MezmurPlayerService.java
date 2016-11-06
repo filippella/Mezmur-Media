efc2c12625a484f6b7dbcca119f08e88ef258fd8
Filippo <filippo.eng@gmail.com>
2016-11-06 20:39:42 +0000

    play mezmur from url in a service

--- app/src/main/java/org/dalol/orthodoxmezmurmedia/modules/player/MezmurPlayerService.java
+++ app/src/main/java/org/dalol/orthodoxmezmurmedia/modules/player/MezmurPlayerService.java
@@ -1,11 +1,18 @@
 package org.dalol.orthodoxmezmurmedia.modules.player;
 
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.widget.Toast;
+
+import org.dalol.orthodoxmezmurmedia.R;
+import org.dalol.orthodoxmezmurmedia.modules.home.MezmurDashboardActivity;
 
 import java.io.IOException;
 
@@ -16,9 +23,13 @@ import java.io.IOException;
  */
 public class MezmurPlayerService extends Service {
 
+    final int NOTIFICATION_ID = 1;
     private MediaPlayer mediaPlayer;
+    private NotificationCompat.Builder mNotificationBuilder;
 
-    @Nullable @Override public IBinder onBind(Intent intent) {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
         return null;
     }
 
@@ -28,19 +39,22 @@ public class MezmurPlayerService extends Service {
         try {
             mediaPlayer = new MediaPlayer();
             mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-            mediaPlayer.setDataSource("url");
+            mediaPlayer.setDataSource("https://raw.githubusercontent.com/filippella/LinksForApp/master/mezmur2.mp3");
+            mediaPlayer.setLooping(false);
             mediaPlayer.prepareAsync();
             //You can show progress dialog here untill it prepared to play
             mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
                 @Override
                 public void onPrepared(MediaPlayer mp) {
                     //Now dismis progress dialog, Media palyer will start playing
+                    showNotification("Started");
                     mp.start();
                 }
             });
             mediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
                 @Override
                 public boolean onError(MediaPlayer mp, int what, int extra) {
+                    Toast.makeText(MezmurPlayerService.this, "Some Error Occurred!", Toast.LENGTH_SHORT).show();
                     // dissmiss progress bar here. It will come here when MediaPlayer
                     //  is not able to play file. You can show error message to user
                     return false;
@@ -53,8 +67,43 @@ public class MezmurPlayerService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        String action = intent.getAction();
+        Toast.makeText(this, action, Toast.LENGTH_SHORT).show();
+//        if (action.equals("play")) play();
 
         return START_STICKY;
+    }
+
+    void showNotification(String text) {
+
+        Intent playerIntent = new Intent(this, MezmursPlayer.class);
+        Intent dashboardIntent = new Intent(getApplicationContext(), MezmurDashboardActivity.class);
+
+        PendingIntent pi = TaskStackBuilder.create(getApplicationContext())
+                .addNextIntentWithParentStack(dashboardIntent)
+                .addNextIntent(playerIntent)
+                .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        mNotificationBuilder = new NotificationCompat.Builder(
+                this).setSmallIcon(R.mipmap.ic_launcher)
+                .addAction(R.mipmap.ic_launcher, "call", getPendingAction("call"))
+                .addAction(R.mipmap.ic_launcher, "more", getPendingAction("more"))
+                .addAction(R.mipmap.ic_launcher, "add more", getPendingAction("add more"))
+                .setContentTitle("My notification")
+                .setContentText("Hello World!")
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .setTicker(text)
+                .setWhen(System.currentTimeMillis())
+                .setContentText(text)
+                .setContentIntent(pi)
+                .setOngoing(true);
+        startForeground(NOTIFICATION_ID, mNotificationBuilder.build());
+    }
+
+    private PendingIntent getPendingAction(String action) {
+        Intent intent = new Intent(getApplicationContext(), MezmurPlayerService.class);
+        intent.setAction(action);
+        return PendingIntent.getService(getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
     @Override
--- app/src/main/java/org/dalol/orthodoxmezmurmedia/modules/player/MezmursPlayer.java
+++ app/src/main/java/org/dalol/orthodoxmezmurmedia/modules/player/MezmursPlayer.java
@@ -2,10 +2,16 @@ package org.dalol.orthodoxmezmurmedia.modules.player;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v4.app.TaskStackBuilder;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import org.dalol.orthodoxmezmurmedia.R;
 import org.dalol.orthodoxmezmurmedia.basic.base.BaseActivity;
+import org.dalol.orthodoxmezmurmedia.modules.home.MezmurDashboardActivity;
+
+import butterknife.OnClick;
 
 /**
  * @author Filippo Engidashet <filippo.eng@gmail.com>
@@ -21,20 +27,62 @@ public class MezmursPlayer extends BaseActivity {
         setTitle("Mezmur Player");
     }
 
+    @OnClick(R.id.start_playing_button)
+    void onPlayClick() {
+        startService(new Intent(this, MezmurPlayerService.class));
+    }
+
+    @OnClick(R.id.stop_playing_button)
+    void onStopClick() {
+        stopService(new Intent(this, MezmurPlayerService.class));
+    }
+
     @Override
     protected int getContentView() {
         return R.layout.activity_mezmurs_player;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Toast.makeText(this, "New Intent Detected", Toast.LENGTH_SHORT).show();
     }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case android.R.id.home:
+//                //NavUtils.navigateUpFromSameTask(this);;
+//                //Intent upIntent = NavUtils.getParentActivityIntent(this);
+//
+//                Intent upIntent = new Intent(this, MezmurDashboardActivity.class);
+//
+//                if (NavUtils.shouldUpRecreateTask(this, upIntent)) {
+//                    // This activity is not part of the application's task, so
+//                    // create a new task
+//                    // with a synthesized back stack.
+//                    TaskStackBuilder
+//                            .from(this)
+//                            .addNextIntent(new Intent(this, MezmurDashboardActivity.class))
+//                            .addNextIntent(upIntent).startActivities();
+//                    finish();
+//                } else {
+//                    // This activity is part of the application's task, so simply
+//                    // navigate up to the hierarchical parent activity.
+//                    NavUtils.navigateUpTo(this, upIntent);
+//                }
+//
+////                if (NavUtils.shouldUpRecreateTask(this, upIntent)) {
+////                    TaskStackBuilder.create(this)
+////                            .addNextIntentWithParentStack(upIntent)
+////                            .startActivities();
+////                } else {
+////                    NavUtils.navigateUpTo(this, upIntent);
+////                }
+//                return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
     @Override
     protected int getStatusBarColor() {
